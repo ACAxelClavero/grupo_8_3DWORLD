56453f59ab42c8ff4bccc48f640821739c8c5251
Emilio Di Fulvio <emiidf1@gmail.com>
2023-09-29 17:20:42 -0300

    actualizacion de estructura del proyecto a ejs: creacion de controller y routes. aplicacion de partials -footer- en product detail, product cart e index

--- src/app.js
+++ src/app.js
@@ -1,30 +1,28 @@
 const express=require("express");
 const path=require("path");
-const port=3017;
+
+const mainRoutes = require('./routes/mainRoutes');
+
 const app=express();
+
 app.use(express.static(path.resolve(__dirname, "public")));
 
-app.get("/", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "views/index.html"));
-});
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 
-app.get("/productCart", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "views/productCart.html"));
-});
+app.use('/', mainRoutes);
 
-app.get("/productDetail", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "views/productDetail.html"));
-});
+app.use('/productCart', mainRoutes);
 
-app.get("/register", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "views/register.html"));
-});
+app.use('/product-detail', mainRoutes);
 
-app.get("/login", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "views/login.html"));
-});
+app.use('/register', mainRoutes); 
 
+app.use('/login', mainRoutes);
+
+
+const port=3017;
 app.listen(port, ()=>{
     console.log(`Servidor iniciado en http://localhost:${port}`);
 });
---
+++ src/routes/mainRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const mainController = require('../controllers/mainController');
+
+const router = express.Router();
+
+router.get('/', mainController.index);
+
+router.get('/login', mainController.login);
+
+router.get('/register', mainController.register);
+
+router.get('/productCart', mainController.productCart);
+
+router.get('/product-detail', mainController.productDetail);
+
+module.exports = router;
