e0c232b33e86e0a5c78cef9717fc5632dc913994
nachob14 <bordonjuanignacio@gmail.com>
2023-11-08 17:15:09 -0300

    Desarrollo de middlewares, rutas y controladores necesarios para el manejo de usuarios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcryptjs');
+
+const { validationResult } = require('express-validator');
 
 const usersFilePath = path.join(__dirname, '../data/users.json');
 
@@ -22,9 +25,20 @@ const controller = {
 
     newUser(req, res){
         const users = getUsers();
+
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.render('register', {
+                errors: errors.mapped(),
+                oldData: req.body
+            });
+        }
+
         const user = {
             id: users[users.length - 1] ? users[users.length - 1].id + 1 : 1,
-            ...req.body
+            ...req.body,
+            imgPerfil : req.file?.filename,
+            password: bcrypt.hashSync(req.body.password, 10),
         };
 
         users.push(user);
@@ -33,7 +47,49 @@ const controller = {
     },
 
     profile(req, res){
-        res.render('/users/profile')
+        const users = getUsers();
+        const user = users.find(element => element.id === req.params.id);
+        if(!user){
+            return res.render('error', {
+                message: 'El usuario no existe',
+                error: {
+                    status: 404
+                },
+                path: req.url
+            });
+        }
+        res.render('/users/profile', { user });
+    },
+
+    edit(req, res){
+        const users = getUsers();
+        const user = users.find(element => element.id === req.params.id)
+        return res.render('users/editUser', { userToEdit: user });
+    },
+
+    update(req, res){
+        const users = getUsers();
+        const userIndex = users.findIndex(element => element.id == req.params.id);
+        const imgPerfil = req.file?.filename || users[userIndex].imgPerfil;
+        users[userIndex] = {
+            ...users[userIndex],   
+            imgPerfil,
+            ...req.body
+        };
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+        res.redirect('/user')
+    },
+
+    delete(req, res){
+        const users = getUsers();
+        const userIndex = users.findIndex(element => element.id == req.params.id);
+        users.splice(userIndex, 1);
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+        res.redirect('/user');
+    },
+
+    loginProcess(req, res){
+        return res.render(req.body);
     }
 }
 
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,20 +1,49 @@
 const express = require('express');
 const path = require('path');
 
+const router = express.Router();
+
+const { createUserValidations } = require('../middlewares/userValidation');
+
+
 const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../public/images/users'));
+    },
+    filename: (req, file, cb) => {
+        let imageName = 'user-' + Date.now() + path.extname(file.originalname);
+        cb(null, imageName);
+    }
+})
+
+const upload = multer({ storage });
 
 const usersController = require('../controllers/usersController');
 
-const router = express.Router();
+/* Obtener usuarios */
+router.get('/', usersController.index);
+
+/* Obtener un usuario especifico */
+router.get('/profile/:id', usersController.profile);
 
 router.get('/login', usersController.login);
 
+
+/* Crear usuario */
 router.get('/register', usersController.register);
+router.post('/register', upload.single('imgPerfil'), createUserValidations, usersController.newUser);
 
-router.post('/register', usersController.newUser);
+/* Editar usuario */
+router.get('/edit/:id', usersController.edit);
+router.put('/:id', upload.single('imgPerfil'), usersController.update);
 
-router.get('/profile', usersController.profile);
+/* Eliminar usuario */
+router.delete('/:id', usersController.delete);
 
+/* Formulario login */
+router.get('/login', usersController.login);
+router.post('/login', usersController.loginProcess)
 
 
 module.exports = router; 
