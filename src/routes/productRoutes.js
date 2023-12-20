bf68d27cfd558e08398d56dc4abd0f0549496e38
AXaxelclavero <axel.m.clavero@gmail.com>
2023-12-20 12:49:49 -0300

    actualizacion

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,16 +1,52 @@
 const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination : (req, file, cb) =>  {
+        cb(null, path.join(__dirname, '../public/img/pitchs'));
+    },
+    filename : (req, file, cb) => {
+        let imageName = 'pitch-' + Date.now() + path.extname(file.originalname);
+        cb(null, imageName);
+    }
+});
+
+const upload = multer({ storage });
+
+
 
 const productController = require('../controllers/productController');
 
-const router = express.Router();
 
+// Obtener listado de productos
+router.get('/', productController.products); 
 
-router.get('/productCart', productController.productCart);
+//Carrito
+router.get('/productCart', productController.productsCart)
 
-router.get('/product-detail', productController.productDetail);
+//Detalle de un producto particular
+router.get('/:id', productController.productDetail); 
 
-router.get('/new-product', productController.newProduct);
+/*** Crear un nuevo producto ***/
+//Formulario de creación de productos
+router.get('/create', productController.create); 
 
-router.get('/edit', productController.editProduct);
+//Acción de creación (a donde se envía el formulario)
+router.post('/', upload.single('img'), productController.newProductCreation)
+
+/*** Editar un producto ***/
+//Formulario de edición de productos
+router.get('/:id/edit-product', productController.editProduct); 
+
+//Acción de edición (a donde se envía el formulario)
+router.put('/:id', upload.single('img'), productController.editProductId); 
+
+//Acción de borrado
+router.delete('/:id', productController.delete);
+
 
 module.exports = router;
+
+
