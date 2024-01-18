607a9f6f6305dfac9a6da3e0455a45c068a1b435
gerisedlar15 <gerisedlar15@gmail.com>
2024-01-18 19:45:50 -0300

    structure.sql2

--- structure1.sql
+++ structure1.sql
@@ -23,12 +23,8 @@ USE `3dword` ;
 CREATE TABLE IF NOT EXISTS `3dword`.`clientes` (
   `Codigo_Cliente` INT(11) NOT NULL AUTO_INCREMENT,
   `Nombre` VARCHAR(45) NOT NULL,
-  `Profesion` VARCHAR(45) NOT NULL,
-  `Matricula` VARCHAR(45) NOT NULL,
-  `Cuit` VARCHAR(45) NOT NULL,
   `Direccion` VARCHAR(45) NOT NULL,
   `Ciudad` VARCHAR(45) NOT NULL,
-  `Celular` INT(11) NOT NULL,
   `Telefono` INT(11) NULL DEFAULT NULL,
   `Mail` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`Codigo_Cliente`))
@@ -37,27 +33,10 @@ DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
--- Table `3dword`.`productos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `3dword`.`productos` (
-  `idProductos` INT(11) NOT NULL,
-  `Nombre` VARCHAR(45) NULL DEFAULT NULL,
-  `Precio` DECIMAL(10,0) NULL DEFAULT NULL,
-  `Fecha` DATE NULL DEFAULT NULL,
-  `Tipo_Plastico` VARCHAR(45) NULL DEFAULT NULL,
-  `Dimensiones` DECIMAL(10,0) NULL DEFAULT NULL,
-  `Color` VARCHAR(45) NULL DEFAULT NULL,
-  `Cantidad` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`idProductos`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
 -- Table `3dword`.`remito`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `3dword`.`remito` (
-  `id_remito` INT(11) NOT NULL,
+  `id_remito` INT(11) NOT NULL AUTO_INCREMENT,
   `Numero` VARCHAR(45) NOT NULL,
   `Fecha` DATETIME NOT NULL,
   `Codigo_Cliente` VARCHAR(45) NOT NULL,
@@ -67,12 +46,36 @@ CREATE TABLE IF NOT EXISTS `3dword`.`remito` (
   `Otros_Importes` DECIMAL(10,0) NOT NULL,
   `Total` DECIMAL(10,0) NOT NULL,
   `remitocol` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_remito`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `3dword`.`productos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `3dword`.`productos` (
+  `idProducto` INT(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `Precio` DECIMAL(10,0) NULL DEFAULT NULL,
+  `Fecha` DATE NULL DEFAULT NULL,
+  `Tipo_Plastico` VARCHAR(45) NULL DEFAULT NULL,
+  `Dimensiones` DECIMAL(10,0) NULL DEFAULT NULL,
+  `Color` VARCHAR(45) NULL DEFAULT NULL,
+  `Cantidad` INT(11) NULL DEFAULT NULL,
   `clientes_Codigo_Cliente` INT(11) NOT NULL,
-  PRIMARY KEY (`id_remito`, `clientes_Codigo_Cliente`),
-  INDEX `fk_remito_clientes1_idx` (`clientes_Codigo_Cliente` ASC) VISIBLE,
-  CONSTRAINT `fk_remito_clientes1`
+  `remito_id_remito` INT(11) NOT NULL,
+  PRIMARY KEY (`idProducto`, `clientes_Codigo_Cliente`, `remito_id_remito`),
+  INDEX `fk_productos_clientes_idx` (`clientes_Codigo_Cliente` ASC) VISIBLE,
+  INDEX `fk_productos_remito1_idx` (`remito_id_remito` ASC) VISIBLE,
+  CONSTRAINT `fk_productos_clientes`
     FOREIGN KEY (`clientes_Codigo_Cliente`)
     REFERENCES `3dword`.`clientes` (`Codigo_Cliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_productos_remito1`
+    FOREIGN KEY (`remito_id_remito`)
+    REFERENCES `3dword`.`remito` (`id_remito`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -83,7 +86,7 @@ DEFAULT CHARACTER SET = utf8mb4;
 -- Table `3dword`.`ventas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `3dword`.`ventas` (
-  `Id_Ventas` INT(11) NOT NULL,
+  `Id_Ventas` INT(11) NOT NULL AUTO_INCREMENT,
   `Codigo_Cliente` VARCHAR(45) NULL DEFAULT NULL,
   `Fecha` DATETIME NULL DEFAULT NULL,
   `Id_Remito` INT(11) NULL DEFAULT NULL,
@@ -92,19 +95,13 @@ CREATE TABLE IF NOT EXISTS `3dword`.`ventas` (
   `Debe` DECIMAL(10,0) NULL DEFAULT NULL,
   `Haber` DECIMAL(10,0) NULL DEFAULT NULL,
   `Saldo` DECIMAL(10,0) NULL DEFAULT NULL,
-  `clientes_Codigo_Cliente` INT(11) NOT NULL,
-  `remito_id_remito` INT(11) NOT NULL,
-  PRIMARY KEY (`Id_Ventas`, `clientes_Codigo_Cliente`, `remito_id_remito`),
-  INDEX `fk_ventas_clientes1_idx` (`clientes_Codigo_Cliente` ASC) VISIBLE,
-  INDEX `fk_ventas_remito1_idx` (`remito_id_remito` ASC) VISIBLE,
-  CONSTRAINT `fk_ventas_clientes1`
-    FOREIGN KEY (`clientes_Codigo_Cliente`)
-    REFERENCES `3dword`.`clientes` (`Codigo_Cliente`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_ventas_remito1`
-    FOREIGN KEY (`remito_id_remito`)
-    REFERENCES `3dword`.`remito` (`id_remito`)
+  `productos_idProducto` INT(11) NOT NULL,
+  `productos_clientes_Codigo_Cliente` INT(11) NOT NULL,
+  PRIMARY KEY (`Id_Ventas`, `productos_idProducto`, `productos_clientes_Codigo_Cliente`),
+  INDEX `fk_ventas_productos1_idx` (`productos_idProducto` ASC, `productos_clientes_Codigo_Cliente` ASC) VISIBLE,
+  CONSTRAINT `fk_ventas_productos1`
+    FOREIGN KEY (`productos_idProducto` , `productos_clientes_Codigo_Cliente`)
+    REFERENCES `3dword`.`productos` (`idProducto` , `clientes_Codigo_Cliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
