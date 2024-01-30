149c63d068046290a6f9c29277c2aa46a0c147ef
AXaxelclavero <axel.m.clavero@gmail.com>
2024-01-30 19:05:42 -0300

    avances en productos

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -10,7 +10,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination : (req, file, cb) =>  {
-        cb(null,  '/public/images/productos');
+        cb(null,path.join(__dirname,  '../public/images/productos'));
     },
     filename : (req, file, cb) => {
         let imageName = 'pitch-' + Date.now() + path.extname(file.originalname);
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage }).array('imagenesProducts');;
 
 
 
@@ -36,14 +36,14 @@ router.get('/:id', productController.productDetail);
 router.get('/new-product', productController.newProductForm); 
 
 //Acción de creación (a donde se envía el formulario)
-router.post('/create', authMiddleware,  upload.single('img'),  productValidator.create, productController.newProductCreation)
+router.post('/create', authMiddleware, upload,  productValidator.create, productController.newProductCreation)
 
 /*** Editar un producto ***/
 //Formulario de edición de productos
 router.get('/:id/edit-product', authMiddleware, productController.editProductForm); 
 
 //Acción de edición (a donde se envía el formulario)
-router.put('/:id', authMiddleware, upload.single('img'), productValidator.edit, productController.editProductId); 
+router.put('/:id', authMiddleware, upload, productValidator.edit, productController.editProductId); 
 
 //Acción de borrado
 router.delete('/:id', authMiddleware, productController.delete);
